7c5d13ccdc0cb8cfeaa6aac9f032fdf262ebb418
Екатерина Яковлева <regspamm@yandex.ru>
2023-07-09 10:30:16 +0300

    create profile edit popup

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
 const popupSection = document.querySelector('.popup.page__section');
+
 const editProfileButton = document.querySelector('.profile__button-edit');
 const closeButton = document.querySelector('.popup__button-close');
-const userField = popupSection.querySelector('input[name="user-name"]');
-const descriptionField = popupSection.querySelector('input[name="user-description"]');
-let userName = document.querySelector('h1.profile__name');
-let userDescription = document.querySelector('p.profile__description');
+const saveButton = document.querySelector('.popup__button-save');
+
+const editProfileForm = document.querySelector('form[name="edit-form"]');
+const nameInput = popupSection.querySelector('input[name="user-name"]');
+const descriptionInput = popupSection.querySelector('input[name="user-description"]');
+
+const userName = document.querySelector('h1.profile__name');
+const userDescription = document.querySelector('p.profile__description');
 
 editProfileButton.addEventListener('click', editProfilePopup);
 closeButton.addEventListener('click', closeEditProfilePopup);
-
-console.log(userName.textContent);
-console.log(userName.textContent);
+saveButton.addEventListener('click', handleFormSubmit);
 
 function closeEditProfilePopup() {
   popupSection.classList.remove('popup_opened');
 }
 
 function editProfilePopup() {
-  userField.value = userName.textContent;
-  descriptionField.value = userDescription.textContent;
+  nameInput.value = userName.textContent;
+  descriptionInput.value = userDescription.textContent;
   popupSection.classList.add('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userDescription.textContent = descriptionInput.value;
+  closeEditProfilePopup();
 }
