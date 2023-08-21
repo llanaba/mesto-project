73a24498f67a84c266f38ebad62b777e847041fb
NotACat1 <notacat.mail@gmail.com>
2023-08-21 14:00:18 +0400

    refactor: creating an Api class
    The Class Api provides services for sending requests and receiving responses from the server.
    Methods:
    1.  _getResponseData - checking the received result from the server for an error;
    2. getUserInfo - downloading user information from the server;
    3. getInitialCards - downloading cards from the server;
    4. getInitialCards - editing a profile;
    5. postNewCard - adding a new card;
    6. deleteCard - deleting a card;
    7. likeCard - setting and removing likes;
    8. updateAvatar - updating the user's avatar.
    
    There is a class creation at the end of the file, but it needs to be taken out of the file index.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,108 +1,121 @@
-const config = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-27',
+export class Api {
+  constructor ({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  // checking the received result from the server for an error
+  // success: decode the result into JSON format
+  // error:   call reject
+  _getResponseData (res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json()
+  }
+
+  // downloading user information from the server
+  async getUserInfo () {
+    const url = `${this._baseUrl}/users/me`;
+    return await fetch(url, {
+      method: 'GET',
+      headers: this._headers
+    })
+      .then((res) => {
+        return this._getResponseData(res);
+      });
+  }
+
+  // downloading cards from the server
+  getInitialCards () {
+    const url = `${this._baseUrl}/cards`;
+    return fetch(url, {
+      method: 'GET',
+      headers: this._headers
+    })
+      .then((res) => {
+        return this._getResponseData(res);
+      });
+  }
+
+  // editing a profile
+  getInitialCards ({ userName, userDescription }) {
+    const url = `${this._baseUrl}/users/me`;
+    return fetch(url, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userName,
+        about: userDescription
+      })
+    })
+      .then((res) => {
+        return this._getResponseData(res);
+      });
+  }
+
+  // adding a new card
+  postNewCard ({ cardName, imageLink }) {
+    const url = `${this._baseUrl}/cards`;
+    return fetch(url, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: cardName,
+        link: imageLink
+      })
+    })
+      .then((res) => {
+        return this._getResponseData(res);
+      });
+  }
+
+  // deleting a card
+  deleteCard ({ cardId }) {
+    const url = `${this._baseUrl}/cards/${cardId}`;
+    return fetch(url, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then((res) => {
+        return this._getResponseData(res);
+      });
+  }
+
+  // setting and removing likes
+  likeCard ({ cardId }, method) {
+    const url = `${this._baseUrl}/cards/likes/${cardId}`
+    return fetch(url, {
+      method: method,
+      headers: this.headers
+    })
+      .then((res) => {
+        return getResponseData(res);
+      })
+  }
+
+  // updating the user's avatar
+  updateAvatar ({ avatarUrl }) {
+    const url = `${this._baseUrl}/users/me/avatar`
+    return fetch(url, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatarUrl
+      })
+    })
+      .then((res) => {
+        return getResponseData(res);
+      })
+  }
+}
+
+
+// !!! Вывести в index.js !!!
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/cohort-42',
   headers: {
-    authorization: 'f064128c-9ee2-488b-af8c-42d65f638aa9',
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
     'Content-Type': 'application/json'
   }
-}
-
-function getResponseData(res) {
-  if (!res.ok) {
-    return Promise.reject(`Ошибка: ${res.status}`);
-  }
-  return res.json()
-}
-
-export const getUser = (id) => {
-  const url = `${config.baseUrl}/users/${id}`;
-  return fetch(url, {
-    headers: config.headers
-  })
-    .then((res) => {
-      return getResponseData(res);
-    });
-}
-
-export const getInitialCards = () => {
-  const url = `${config.baseUrl}/cards`;
-  return fetch(url, {
-    headers: config.headers
-  })
-    .then((res) => {
-      return getResponseData(res);
-    });
-}
-
-export const updateProfileInfo = (userName, userDescription) => {
-  const url = `${config.baseUrl}/users/me`;
-  return fetch(url, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: userName,
-      about: userDescription
-    })
-  })
-    .then((res) => {
-      return getResponseData(res);
-    });
-}
-
-export const postNewCard = (cardName, imageLink) => {
-  const url = `${config.baseUrl}/cards`;
-  return fetch(url, {
-    method: 'POST',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: cardName,
-      link: imageLink
-    })
-  })
-  .then((res) => {
-    return getResponseData(res);
-  });
-}
-
-export const deleteCard = (cardId) => {
-  const url = `${config.baseUrl}/cards/${cardId}`
-  return fetch(url, {
-    method: 'DELETE',
-    headers: config.headers,
-  })
-    .then((res) => {
-      return getResponseData(res);
-    })
-}
-
-export const likeCard = (cardId, method) => {
-  const url = `${config.baseUrl}/cards/likes/${cardId}`
-  return fetch(url, {
-    method: method,
-    headers: config.headers,
-  })
-  .then((res) => {
-    return getResponseData(res);
-  })
-}
-
-export const updateAvatar = (avatarUrl) => {
-  const url = `${config.baseUrl}/users/me/avatar`
-  return fetch(url, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      avatar: avatarUrl
-    })
-  })
-  .then((res) => {
-    return getResponseData(res);
-  })
-}
-
-export const renderLoading = (isLoading, saveButton, origText) => {
-  if (isLoading) {
-    saveButton.innerText = "Сохранение...";
-  } else {
-    saveButton.innerText = origText;
-  }
-}
+}); 
