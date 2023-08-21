7d68867c58d618320ccc4d16740e01ac49ffb8d1
Екатерина Яковлева <regspamm@yandex.ru>
2023-08-21 23:44:43 +0300

    add class FormValidator

---
+++ src/components/FormValidator.js
@@ -0,0 +1,68 @@
+export default class FormValidator {
+  constructor ({ inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass }, form) {
+    this._inputList = form.querySelectorAll(inputSelector);
+    this._submitButton = form.querySelector(submitButtonSelector);
+    this._inactiveButtonClass = inactiveButtonClass;
+    this._inputErrorClass = inputErrorClass;
+    this._errorClass = errorClass;
+  }
+
+  // checking all inputs for validity
+  _hasInvalidInput () {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  // enabling and disabling the submit button
+  _toggleButtonState () {
+    if (this._hasInvalidInput()) {
+      this._submitButton.disabled = true;
+      this._submitButton.classList.add(this._inactiveButtonClass);
+    } else {
+      this._submitButton.disabled = false;
+      this._submitButton.classList.remove(this._inactiveButtonClass);
+    }
+  }
+
+  // output of the input error message
+  _showInputError (inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._errorClass);
+  }
+
+  // hiding the input error message
+  _hideInputError (inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = '';
+  }
+
+  // validation of input
+  _isValid (inputElement) {
+    if (inputElement.validity.patternMismatch) {
+      inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    } else {
+      inputElement.setCustomValidity('');
+    }
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
+    }
+  }
+
+  // disabling the listener for each input with validation + enabling and disabling the submit button
+  setEventListeners () {
+    this._toggleButtonState();
+    this._inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', () => {
+        this._isValid(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+}
