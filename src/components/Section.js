f0e4f7e1b52ab422167f1506c23b229a318ad2d8
NotACat1 <notacat.mail@gmail.com>
2023-08-22 12:34:12 +0400

    fix: correction of the Section class
    1. renderItems - fixing the method so that it creates and returns a ready-made markup.

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,23 +1,21 @@
 export default class Section {
-  constructor({ items, renderer }, containerSelector) {
+  constructor ({ items, renderer }, containerSelector) {
     this._renderedItems = items;
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
-  addItem(element) {
+  // adding markup to a container
+  addItem (element) {
     this._container.append(element);
   }
 
-  // clear() {
-  //   this._container.innerHTML = '';
-  // }
-
-  renderItems() {
-    // this.clear();
-
+  // creating element markup
+  renderItems () {
+    const rezItems = '';
     this._renderedItems.forEach(item => {
-      this._renderer(item);
+      rezItems.after(this._renderer(item));
     });
+    return rezItems;
   }
 }
