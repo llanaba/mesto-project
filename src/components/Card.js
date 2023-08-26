af220c62d02be4923abe69f18d888286da7e8509
NotACat1 <notacat.mail@gmail.com>
2023-08-26 22:47:46 +0400

    build: adding card deletion functionality
    - сomments have been added
    - errors and bugs have been fixed
    - the functionality for deleting a photo card has also been added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,102 +1,101 @@
 import { cardSelectors } from "../utils/constants";
+
 export default class Card {
-  constructor(
-    {_id, name, link, owner, likes},
+  constructor (
+    { _id, name, link, owner, likes },
     cardTemplateSelector,
-    { deleteCardApi, likeCardApi, openViewImagePopup }
+    { deleteCard, likeCardApi, openViewImagePopup }
   ) {
-    this._selector = cardTemplateSelector
+    this._cardId = _id; // photo card id
+    this._name = name; // photo card name
+    this._link = link; // link to the image of the photo card
+    this._owner = owner; // information about the author of the photo card
+    this._likes = likes; // list of people who liked the card
 
-    this._cardId = _id;
-    this._name = name;
-    this._link = link;
-    this._owner = owner;
-    this._likes = likes;
+    this._selector = cardTemplateSelector // photo card template selector
 
-    this._deleteCard = deleteCardApi;
-    this._likeCard = likeCardApi;
-    this._viewImage = openViewImagePopup;
-    // this._appendCard = appendCard;
+    this._deleteCard = deleteCard; // photo card removal function
+    this._likeCard = likeCardApi; // like function photo cards
+    this._viewImage = openViewImagePopup; // the function of opening a pop-up with the image of a photo card
+
+    this._userAuthor = false; // is the author of the photo card a user
+    this._userLike = false; // did the user like the photos
   }
 
-  // пользуясь селектором template-элемента, создает пустой элемент карточки
-  // и возвращает его
-  _getElement() {
-    const cardElement = document.querySelector(this._selector).content
-      .querySelector('.card').cloneNode(true);
-    return cardElement
+  // return of the finished marking of the photo card
+  _getElement () {
+    return document
+      .querySelector(this._selector)
+      .content
+      .querySelector('.card')
+      .cloneNode(true);
   }
 
-  _userLikesCard (userId) {
-    if (this._likes) {
-      const userLikes = this._likes.some(function(fan) {
-        return fan._id === userId
-      });
-      return userLikes
-    }
-    return false
-  }
-
+  // deleting a photo card
   _handleDeleteClick () {
-    this._deleteCard(this._cardId)
-      .then((res) => {
-        this._element.remove();
-      });
+    this._deleteCard(this);
   }
 
-  _handleLikeClick(evt, cardId) {
-    let method
-    if (evt.target.classList.contains('card__button-like_active')) {
-      method = 'DELETE'
-    } else {
-      method = 'PUT'
-    }
-    this._likeCard(cardId, method)
-      .then((cardData) => {
-        const likes = evt.target.nextElementSibling;
-        likes.textContent = cardData.likes.length;
-        evt.target.classList.toggle('card__button-like_active');
+  // the layout of the like of the photo card
+  _handleLikeClick () {
+    const method = this._userLike ? 'DELETE': 'PUT';
+    this._likeCard(this._cardId, method)
+      .then(({ likes }) => {
+        this._userLike = !this._userLike;
+        this._likes = likes;
+        this._element.likeCounter.textContent = this._likes.length;
+        this._element.buttons.buttonLikeElement.classList.toggle('card__button-like_active');
       })
       .catch((err) => {
         console.log(err);
-      })
+      });
   }
 
-  _setEventListeners() {
-    this._buttonLikeElement.addEventListener('click', (evt) => {
-        this._handleLikeClick(evt, this._cardId)
-      })
-    this._buttonBinElement.addEventListener('click', (evt) => {
+  // the layout of the like of the photo card
+  _setEventListeners () {
+    this._element.buttons.buttonLikeElement.addEventListener('click', (evt) => {
+      this._handleLikeClick();
+    });
+    this._element.buttons.buttonBinElement.addEventListener('click', (evt) => {
       this._handleDeleteClick();
-    })
-    this._element.querySelector(cardSelectors.cardImage).addEventListener('click', (evt) => {
+    });
+    this._element.cardImage.addEventListener('click', (evt) => {
       this._viewImage(this._name, this._link);
-    })
+    });
   }
-  // возвращает полностью готовый элемент карточки
+  
+  // returns a fully finished card element
   generate({ _id: userId }) {
+    if (this._owner._id === userId) this._userAuthor = true;
+    if (this._likes.some((like) => like === userId)) this._userLike = true;
+
     this._element = this._getElement();
 
-    // отрисовываем то, что не зависит от юзера
-    this._element.querySelector(cardSelectors.cardName).textContent = this._name;
-    this._element.querySelector(cardSelectors.cardImage).src = this._link;
-    this._element.querySelector(cardSelectors.cardImage).alt = this._name;
-    this._element.querySelector(cardSelectors.likeCounter).textContent = this._likes.length;
+    // we draw something that does not depend on the user
+    this._element.cardName = this._element.querySelector(cardSelectors.cardName);
+    this._element.cardImage = this._element.querySelector(cardSelectors.cardImage);
+    this._element.likeCounter = this._element.querySelector(cardSelectors.likeCounter);
 
-    // ищем кнопки для карточки
-    this._buttonBinElement = this._element.querySelector(cardSelectors.buttonBin)
-    this._buttonLikeElement = this._element.querySelector(cardSelectors.buttonLike)
+    this._element.cardName.textContent = this._name;
+    this._element.cardImage.src = this._link;
+    this._element.cardImage.alt = this._name;
+    this._element.likeCounter.textContent = this._likes.length;
 
-    // отрисовываем сердечко в зависимости от того, лайкнуто ли оно юзером
-    if (this._userLikesCard(userId)) {
-        this._buttonLikeElement.classList.add('card__button-like_active')
+    // looking for buttons for the card
+    this._element.buttons = {};
+    this._element.buttons.buttonLikeElement = this._element.querySelector(cardSelectors.buttonLike);
+    this._element.buttons.buttonBinElement = this._element.querySelector(cardSelectors.buttonBin);
+
+    // we draw a heart depending on whether it is liked by the user
+    if (this._userLike) {
+      this._element.buttons.buttonLikeElement.classList.add('card__button-like_active')
+    }
+    // we draw the basket depending on whether the user is the author
+    if (this._userAuthor) {
+      this._element.buttons.buttonBinElement.style.display = 'block';
     }
 
-    // отрисовываем корзину в зависимости от того, является ли юзер автором
-    if (userId === this._owner._id || this._owner._id === undefined) {
-      this._buttonBinElement.style.display = "block";
-    }
-    // вешаем слушалки
+    // hang up the listeners
     this._setEventListeners();
     return this._element;
   }
