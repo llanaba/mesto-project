de965437e34cad812a3661df70f1cfc038ba6a4c
NotACat1 <notacat.mail@gmail.com>
2023-08-26 22:50:50 +0400

    docs: adding comments

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,10 +1,10 @@
 import Popup from './Popup.js';
 
 export default class PopupWithImage extends Popup {
-  constructor(selector) {
+  constructor (selector) {
     super(selector);
-    this._image = this._element.querySelector('.figure__image');
-    this._caption = this._element.querySelector('.figure__caption');
+    this._image = this._element.querySelector('.figure__image'); // link to the image for the popup
+    this._caption = this._element.querySelector('.figure__caption'); // image descriptions
 	}
 
   // redefines parent 'open' method: adds image and caption to the popup
