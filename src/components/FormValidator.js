8e91110b07c140792cc51c106385fd7c1bc60003
NotACat1 <notacat.mail@gmail.com>
2023-08-26 22:47:46 +0400

    build: adding card deletion functionality
    - сomments have been added
    - errors and bugs have been fixed
    - the functionality for deleting a photo card has also been added

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -1,11 +1,11 @@
 export default class FormValidator {
   constructor ({ inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass }, form) {
-    this._inputList = [...form.querySelectorAll(inputSelector)];
-    this._submitButton = form.querySelector(submitButtonSelector);
-    this._inactiveButtonClass = inactiveButtonClass;
-    this._inputErrorClass = inputErrorClass;
-    this._errorClass = errorClass;
-    this._form = form;
+    this._inputList = [...form.querySelectorAll(inputSelector)]; // array of input form elements
+    this._submitButton = form.querySelector(submitButtonSelector); // submit button element
+    this._inactiveButtonClass = inactiveButtonClass; // the class of the active submit button
+    this._inputErrorClass = inputErrorClass; // invalid input field class
+    this._errorClass = errorClass; // comment class for invalid input field
+    this._form = form; // form element
   }
 
   // checking all inputs for validity
