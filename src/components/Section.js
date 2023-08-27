fc9c66de13f38194537343210879e69c66f75c47
Екатерина Яковлева <regspamm@yandex.ru>
2023-08-27 11:23:29 +0300

    fix: order of rendering cards

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,6 +1,6 @@
 export default class Section {
   constructor ({ items, renderer }, containerSelector) {
-    this._renderedItems = items; // array with photo card information
+    this._renderedItems = items.reverse(); // array with photo card information
     this._renderer = renderer; // function for creating photo cards
     this._container = document.querySelector(containerSelector); // the element in which are placed
   }
