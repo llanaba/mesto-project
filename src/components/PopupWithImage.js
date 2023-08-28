78ab4e5d6d6d7fd349b76368342ce1c2eedae3dd
NotACat1 <notacat.mail@gmail.com>
2023-08-28 18:15:28 +0400

    fix: bug with the display of the previous image
    Fixing a bug related to the display of the previous open image.
    Solution: when closing the popup, delete the fields with information about the previous image (placed in the '_resetInfo' method).
    
    There is also a solution via '.onload' images. Solution: open the popup only when the image is loaded. Lines - 23-27

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -7,12 +7,31 @@ export default class PopupWithImage extends Popup {
     this._caption = this._popupElement.querySelector('.figure__caption'); // image descriptions
 	}
 
+  // resetting image information
+  _resetInfo () {
+    this._image.src = '';
+    this._image.alt = '';
+    this._caption.textContent = '';
+  }
+
   // redefines parent 'open' method: adds image and caption to the popup
   open (name, img) {
     this._image.src = img;
     this._image.alt = name;
     this._caption.textContent = name;
 
+    // code example with .onload
+    //const openPopup = () => super.open();
+    //this._image.onload = function () {
+    //  openPopup();
+    //}
+
     super.open();
+  }
+
+  // closing the popup and resetting the image information
+  close () {
+    super.close();
+    this._resetInfo();
   }
 }
