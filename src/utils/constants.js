86f9d19ef32770939176303d5d9378f777b8e80b
NotACat1 <notacat.mail@gmail.com>
2023-08-28 18:20:12 +0400

    build: added constants: error time and form names
    1. timeErr - time in seconds, how long the submit error is displayed;
    2. nameForms - object with form names

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,14 +5,14 @@ export const config = {
     authorization: 'f064128c-9ee2-488b-af8c-42d65f638aa9',
     'Content-Type': 'application/json'
   }
-}
+};
 
 // user profile selectors
 export const userSelectors = {
   nameSelector: 'h1.profile__name',
   infoSelector: 'p.profile__description',
   avatarSelector: '.profile__avatar'
-}
+};
 
 // popup selectors
 export const popupSelectors = {
@@ -21,7 +21,7 @@ export const popupSelectors = {
   editAvatar: '.popup_edit-avatar',
   viewImage: '.popup_view-image',
   deleteCard: '.popup_confirm-delete'
-}
+};
 
 // photo card selectors
 export const cardSelectors = {
@@ -30,7 +30,7 @@ export const cardSelectors = {
   likeCounter: '.card__like-counter',
   buttonBin: '.card__button-bin',
   buttonLike: '.card__button-like'
-}
+};
 
 // selectors for form validation
 export const validationSelectors = {
@@ -40,11 +40,21 @@ export const validationSelectors = {
   inactiveButtonClass: 'popup__button-save_disabled',
   inputErrorClass: 'form__input-text_error',
   errorClass: 'form__input-error_active'
-}
+};
 
 // button selectors
 export const buttons = {
   addCard: document.querySelector('.profile__button-add'),
   editProfile: document.querySelector('.profile__button-edit'),
   changeAvatar: document.querySelector('.profile__avatar-overlay'),
-}
+};
+
+// time to reset the error
+export const timeErr = 4; // seconds
+
+// form names
+export const nameForms = {
+  editProfile: 'edit-profile-form',
+  createCard: 'new-card-form',
+  editAvatar: 'edit-avatar-form'
+};
